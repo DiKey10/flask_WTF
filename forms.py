a69ec9c4cf3fd0844dde461b7f9e798be8e551cb
DiKey10 <kd930603@gmail.com>
2022-09-27 10:51:21 +0200

    update

---
+++ forms.py
@@ -0,0 +1,44 @@
+import json
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Email
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+
+class EmailPasswordForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+
+
+
+class Todos:
+    def __init__(self):
+        try:
+            with open("todos.json", "r") as f:
+                self.todos = json.load(f)
+        except FileNotFoundError:
+            self.todos = []
+
+    def all(self):
+        return self.todos
+
+    def get(self, id):
+        return self.todos[id]
+
+    def create(self, data):
+        data.pop('csrf_token')
+        self.todos.append(data)
+
+    def save_all(self):
+        with open("todos.json", "w") as f:
+            json.dump(self.todos, f)
+
+    def update(self, id, data):
+        data.pop('csrf_token')
+        self.todos[id] = data
+        self.save_all()
+
+
+todos = Todos()
