536f111a48d713d2e5d2cbf7f9876da7d937c829
DiKey10 <kd930603@gmail.com>
2022-10-03 11:44:19 +0200

    update +1

--- forms.py
+++ forms.py
@@ -1,6 +1,5 @@
-import json
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField ,BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Email
 
 class LoginForm(FlaskForm):
@@ -11,34 +10,7 @@ class EmailPasswordForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
 
-
-
-class Todos:
-    def __init__(self):
-        try:
-            with open("todos.json", "r") as f:
-                self.todos = json.load(f)
-        except FileNotFoundError:
-            self.todos = []
-
-    def all(self):
-        return self.todos
-
-    def get(self, id):
-        return self.todos[id]
-
-    def create(self, data):
-        data.pop('csrf_token')
-        self.todos.append(data)
-
-    def save_all(self):
-        with open("todos.json", "w") as f:
-            json.dump(self.todos, f)
-
-    def update(self, id, data):
-        data.pop('csrf_token')
-        self.todos[id] = data
-        self.save_all()
-
-
-todos = Todos()
+class TodoForm(FlaskForm):
+    title = StringField('title', validators=[DataRequired()])
+    description = TextAreaField('description', validators=[DataRequired()])
+    done = BooleanField('done')
